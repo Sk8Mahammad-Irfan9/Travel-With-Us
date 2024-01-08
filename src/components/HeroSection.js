7eaee555c747f0429cdce10d3b5de24a78efded0
Sk Mahammad Irfan <109087483+Sk8Mahammad-Irfan9@users.noreply.github.com>
2024-01-08 10:56:28 +0530

    Second commit

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import "./HeroSection.css";
 const HeroSection = () => {
   return (
     <div className="hero-container">
-      {/* <video src="/videos/video-1.mp4" autoPlay loop muted /> */}
+      <video src="/videos/video-1.mp4" autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className="hero-btns">
@@ -19,13 +19,13 @@ const HeroSection = () => {
         </Button>
       </div>
 
-      <Button
+      {/* <Button
         className="btns"
         buttonStyle="btn--primary"
         buttonSize="btn--large"
       >
         WATCH TRAILER <i className="far fa-play-circle" />
-      </Button>
+      </Button> */}
     </div>
   );
 };
